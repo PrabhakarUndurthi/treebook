906762c975a1a22fc2a2c1dc2cd755febd32145e
prabhakar <undurthi.prabhakar@gmail.com>
2014-05-26 22:22:18 +0530

    Initial commit for treebook.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '26e1b1c3529cae53c476a1ab165a0b499a3d587c3144c81884c3f3c61cfa66b89080382be5bdc18ccfd97d3b854a8d0d86336a2d0e46f87b414c1f24816eaf73'
